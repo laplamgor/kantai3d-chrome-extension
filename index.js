22254ab6dc6d0eac4bdbd0a093be274c00c11631
laplamgor <laplamgor@gmail.com>
2021-08-01 14:08:16 +0800

    Update to v1.1

--- index.js
+++ index.js
@@ -47,19 +47,17 @@ function patchMainJs(contents) {
 window.portOffset = -window.charal + window.charah.x;//-l+h.x
 window.portOffsetR = window.charar;//r
 
-window.displacementSprite = PIXI.Sprite.fromImage('https://kantai3d.com/'+ window.displacementPath );
+window.displacementSprite = PIXI.Sprite.fromImage(window.displacementPath.replace(/resources\\/ship\\/full\\/([0-9]*)_([0-9_a-z]*).png(\\?version=)?([0-9]*)/g, \"https://cdn.jsdelivr.net/gh/laplamgor/kantai3d-depth-maps@master/source/$$1/$$1_$$2_v$$4.png\"));
 window.displacementFilter = PIXI.DepthPerspectiveFilter;
 
 window.displacementFilter.uniforms.textureWidth = this._chara.texture.width;
 window.displacementFilter.uniforms.textureHeight = this._chara.texture.height;
-window.displacementFilter.padding = 0;
-
 
 window.displacementSprite.visible = false;
 
 window.displacementFilter.padding = 150;
 
-window.currenctChara = this._chara;
+window.currentChara = this._chara;
 
 if (window.displacementSprite.width != 1) {
     // The depth map is already loaded
@@ -67,21 +65,21 @@ if (window.displacementSprite.width != 1) {
     window.displacementFilter.uniforms.scale = 1.0;
     window.displacementFilter.uniforms.focus = 0.5;
     window.displacementFilter.uniforms.offset = [0.0, 0.0];
-    window.currenctChara.filters = [window.displacementFilter];
-    window.currenctChara.addChild(window.displacementSprite);
+    window.currentChara.filters = [window.displacementFilter];
+    window.currentChara.addChild(window.displacementSprite);
 } else {
     // The depth map is not loaded yet, and may not exist in server at all
     // Disable the filter first
     this._chara.filters = [];
-    window.currenctChara.filters = [];
+    window.currentChara.filters = [];
     window.displacementSprite.texture.baseTexture.on('loaded', function(){
         // Re-enable the filter when resource loaded
         window.displacementFilter.uniforms.displacementMap = window.displacementSprite.texture;
         window.displacementFilter.uniforms.scale = 1.0;
         window.displacementFilter.uniforms.focus = 0.5;
         window.displacementFilter.uniforms.offset = [0.0, 0.0];
-        window.currenctChara.filters = [window.displacementFilter];
-        window.currenctChara.addChild(window.displacementSprite);
+        window.currentChara.filters = [window.displacementFilter];
+        window.currentChara.addChild(window.displacementSprite);
     });
 }
 `);
@@ -91,21 +89,55 @@ if (window.displacementSprite.width != 1) {
 
     contents = contents.replace(/(this\['y'\]=this\[.{0,99}\('.{0,99}'\)]-1.5\*.{0,99}\*1.8;)/g, 
         `$1
-var mousex = (window.pixiApp.renderer.plugins.interaction.mouse.global.x/1200.0-0.5);
-var mousey = (window.pixiApp.renderer.plugins.interaction.mouse.global.y/720.0-0.5);
+var mousex2 = (window.pixiApp.renderer.plugins.interaction.mouse.global.x);
+var mousey2 = (window.pixiApp.renderer.plugins.interaction.mouse.global.y);
+
+if (!mouseoutside) {
+    if (!window.mousex1) window.mousex1 = mousex2;
+    if (!window.mousey1) window.mousey1 = mousey2;
+
+    if (!window.mousex) window.mousex = 0;
+    if (!window.mousey) window.mousey = 0;
+    if (!window.mouset) window.mouset = Date.now();
+
+    window.mousex += (mousex2 - window.mousex1) * (Date.now() - window.mouset);
+    window.mousey += (mousey2 - window.mousey1) * (Date.now() - window.mouset);
+
+    window.mousex1 = mousex2;
+    window.mousey1 = mousey2;
+}
 
 window.displacementFilter.uniforms.textureScale = this.scale.x;
 
 var flip = (this.parent._chara.transform.position.x - window.portOffset) / (window.portOffsetR) - 0.5;
-window.displacementFilter.uniforms.offset = [flip * mousex *1.3
-,0.02 * window.charasin - 0.01 + mousey * 0.6];
+window.displacementFilter.uniforms.offset = [flip * (window.mousex / 1200.0 * 0.05) *1.3
+,0.02 * window.charasin - 0.01 + (window.mousey / 720.0 * 0.05) * 0.6];
 
+
+window.mousex = window.mousex * Math.pow(0.9, (Date.now() - window.mouset) / 30.0);
+window.mousey = window.mousey * Math.pow(0.9, (Date.now() - window.mouset) / 30.0);
+window.mouset = Date.now();
 `);
 
     return contents + `;
 
+
+
+document.addEventListener('mouseleave', e => {
+    window.mouseoutside = true;
+    window.mousex1 = null;
+    window.mousey1 = null;
+});
+
+document.addEventListener('mouseenter', e => {
+    window.mouseoutside = false;
+});
+
+
+
+
 'use strict';
-PIXI.DepthPerspectiveFilter = new PIXI.Filter(\``+ vert + `\`, \`` + frag + `\`);
+PIXI.DepthPerspectiveFilter = new PIXI.Filter(null, \`` + frag + `\`);
 
 PIXI.DepthPerspectiveFilter.apply = function(filterManager, input, output)
 {
@@ -122,34 +154,6 @@ PIXI.DepthPerspectiveFilter.apply = function(filterManager, input, output)
   filterManager.applyFilter(this, input, output);
 }
 
-
-// var isloaded = false;
-//   var alpha    = 0;
-//   var beta     = 0;
-//   var gamma    = 0;
-
-
-// window.addEventListener("deviceorientation", handleOrientation, true);
-
-// function handleOrientation(e) {
-//   if (!isloaded) {
-//     alpha = e.alpha;
-//     beta = e.beta;
-//     gamma = e.gamma;
-//     isloaded = true;
-//    }
-//   console.log("ha" + e.alpha);
-
-//   if (window.displacementFilter && window.displacementFilter.uniforms && window.displacementFilter.uniforms.offset) {
-//     window.displacementFilter.uniforms.offset =  [(window.displacementFilter.uniforms.offset[0] + (e.alpha - alpha)/180.0) * 0.5, 
-//     (window.displacementFilter.uniforms.offset[1] + (e.beta - beta)/180.0) * 0.5];
-
-//     console.log((window.displacementFilter.uniforms.offset[0] + (e.alpha - alpha)/180.0) * 0.5);
-//   }
-//   alpha = e.alpha;
-//   beta = e.beta;
-//   gamma = e.gamma;
-// }
 `;
 }
 
@@ -340,20 +344,4 @@ void main(void)
                        uSampler,
                        (posSum - posSumLast) * -clamp(weigth * 0.5 + 0.5, 0.0, 1.5) + posSum);
 
-}`;
-
-let vert = `#ifdef GL_ES
-precision highp float;
-#endif
-
-attribute vec2 aVertexPosition;
-attribute vec2 aTextureCoord;
-varying vec2 vTextureCoord;
-
-uniform mat3 projectionMatrix;
-
-void main(void)
-{
-    vTextureCoord = aTextureCoord;
-    gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
 }`;
